7705044f4e7d7df09c36d4b2abcc4335d8409762
Rachel Gould <rachel@rachelgould.dev>
2020-04-13 13:19:10 -0700

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 
-const loginLink = 'https://auth.gamesnightonline.com/login?client_id=19riu4d4pv8euflkqtnnl84o2h&response_type=code&scope=aws.cognito.signin.user.admin&redirect_uri=http://localhost:3000/api/loggedin'
 const Home = () => (
   <div className="container">
     <Head>
@@ -18,36 +17,6 @@ const Home = () => (
           Log in
         </a>
       </p>
-
-      {/* <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div> */}
     </main>
 
     <footer>
